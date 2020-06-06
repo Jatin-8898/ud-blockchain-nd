b08a888e89446db2e5c8eb3e447d48c336971868
Jatin-8898 <varlyanijatin88@gmail.com>
2020-06-06 19:00:05 +0530

    Created Readme and added snips

--- Supply Chain Dapp/contracts/coffeeaccesscontrol/FarmerRole.sol
+++ Supply Chain Dapp/contracts/coffeeaccesscontrol/FarmerRole.sol
@@ -21,7 +21,7 @@ contract FarmerRole {
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
   modifier onlyFarmer() {
-    require(isFarmer(msg.sender));
+    require(isFarmer(msg.sender), "Only Farmer is allowed to perform this operation");
     _;
   }
 

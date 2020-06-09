774b0a28d16db8246d359734a316f44243f81870
Jatin-8898 <varlyanijatin88@gmail.com>
2020-06-09 19:00:36 +0530

    Modified to solve the errors in deploying

--- Blockchain Capstone/eth-contracts/migrations/2_deploy_contracts.js
+++ Blockchain Capstone/eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 // migrating the appropriate contracts
-var SquareVerifier = artifacts.require("./SquareVerifier.sol");
+//var SquareVerifier = artifacts.require("./SquareVerifier.sol");
+var SquareVerifier = artifacts.require("verifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
+module.exports = async function(deployer) {
+  await deployer.deploy(SquareVerifier);
+  //deployer.deploy(SolnSquareVerifier);
+  await deployer.deploy(SolnSquareVerifier, SquareVerifier.address, "Div_ERC721MintableToken", "DIV_EC_721");
 };
